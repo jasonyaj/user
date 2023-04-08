8251d2da371715d4c3ae4f3ea4ba48359ad0f61f
jasonyaj <jason.yaj@gmail.com>
2023-04-08 18:41:09 -0500

    completed both bonus

--- chaining_methods.py
+++ chaining_methods.py
@@ -1,4 +1,7 @@
+# copy of user, adding in chaining and 2 ninja bonus
 class User:
+    list_of_users = []
+
     def __init__(self, first_name, last_name, email, age):
         self.first_name = first_name
         self.last_name = last_name
@@ -7,35 +10,50 @@ class User:
         # defaulted attributes
         self.is_rewards_member = False
         self.gold_card_points = 0
+        User.list_of_users.append(self)
+
 
     # Methods
     def display_info(self):
-        first_name = self.first_name
-        last_name = self.last_name
-        email = self.email
-        age = self.age
-        is_rewards_member = self.is_rewards_member
-        gold_card_points = self.gold_card_points
-        print(first_name)
-        print(last_name)
-        print(email)
-        print(age)
-        print(is_rewards_member)
-        print(gold_card_points)
+        print(f'First name: {self.first_name}')
+        print(f'Last name: {self.last_name}')
+        print(f'Email: {self.email}')
+        print(f'Age: {self.age}')
+        print(f'Is a rewards member: {self.is_rewards_member}')
+        print(f'Points: {self.gold_card_points}')
+        print('------------------------------')
         return self
 
     def enroll(self):
-        self.is_rewards_member = True
-        self.gold_card_points = 200
+        for user in User.list_of_users:
+            if user.is_rewards_member == True:
+                print("User already a  member")
+                return False
+            else:
+                self.is_rewards_member = True
+                self.gold_card_points = 200
         return self
 
     def spend_points(self, amount):
+        for user in User.list_of_users:
+            if user.gold_card_points < amount:
+                print('Insufficient points')
+                return False
         self.gold_card_points = self.gold_card_points - amount
         return self
 
+    @classmethod
+    def print_all_users( cls ):
+        for user in cls.list_of_users:
+            user.display_info()
+
+
 
 jason = User('Jason', 'Yang', 'jason@email.com', '35')
-nick = User('Nicholas', 'Yang', 'nick@email.com', '14')
-maddy = User('Madeline', 'Moua', "maddy@email.com", '6')
+# nick = User('Nicholas', 'Yang', 'nick@email.com', '14')
+# maddy = User('Madeline', 'Moua', "maddy@email.com", '6')
 
-jason.enroll().spend_points(50).display_info()
+jason.enroll()
+jason.enroll()
+jason.spend_points(50)
+jason.display_info()
--- user.py
+++ user.py
@@ -1,3 +1,4 @@
+# original copy
 class User:
     def __init__(self, first_name, last_name, email, age):
         self.first_name = first_name
